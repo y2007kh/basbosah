c506e6bbfb7aab144229c3f9f5396701082ed755
y2007kh <y2007kh@gmail.com>
2025-12-30 23:58:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,4 +101,14 @@ function butterflies(x, y) {
         audioArea.classList.add("show");
     });
 
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '-250px') {
+        sidebar.style.left = '0';
+    } else {
+        sidebar.style.left = '-250px';
+    }
+}
+
+
 
